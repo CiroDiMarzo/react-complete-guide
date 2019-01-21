6ce5fd45399abc94ae445e63eb4fc0a735bd604b
unknown <ciro.dimarzo@gmail.com>
2019-01-21 10:40:48 +0100

    added the cockpit component

---
+++ src/components/Cockpit/Cockpit.js
@@ -0,0 +1,34 @@
+import React from "react";
+import classes from './Cockpit.css'
+
+const cockpit = (props) => {
+
+    const assignedClasses = [];
+    let buttonClass = null;
+
+    if (props.showPersons) {
+        buttonClass = classes.Red;
+    }
+
+    if (props.persons.length <= 2) {
+      assignedClasses.push(classes.red);
+    }
+    if (props.persons.length <= 1) {
+      assignedClasses.push(classes.bold);
+    }
+
+    let className = assignedClasses.join(' ');
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1>{props.appTitle}</h1>
+            <p className={className}>This is really working!</p>
+            <button 
+                onClick={props.clicked}
+                className={buttonClass}>
+                Toggle Persons
+            </button>
+        </div>);
+};
+
+export default cockpit;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
 import classes from './App.css';
-import Person from "../components/Persons/Person/Person";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: 'rwe', name: 'Max', age: 28},
-      { id: 'few', name: 'Manu', age: 29},
-      { id: 'qwe', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some other value',
-    showPersons: false
-  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        { id: 'rwe', name: 'Max', age: 28},
+        { id: 'few', name: 'Manu', age: 29},
+        { id: 'qwe', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some other value',
+      showPersons: false
+    };
+
+    // never reach out to a web server during the constructor execution
+  }
 
   nameChangedHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(
@@ -21,11 +28,8 @@ class App extends Component {
     const person = {
       ...this.state.persons[personIndex]
     };
-
     //const person = Object.assign({}, this.state.persons[personIndex]);
-
     person.name = event.target.value;
-
     const persons = [...this.state.persons];
     persons[personIndex] = person;
 
@@ -52,47 +56,21 @@ class App extends Component {
 
   render() {
     let persons = null;
-    let buttonClass = null;
     if (this.state.showPersons) {
       persons = (
-        <div>
-          {
-            this.state.persons.map((person, index) => {
-              return (
-                <Person 
-                  click={this.deletePersonHandler.bind(this, index)}
-                  name={person.name}
-                  age={person.age}
-                  key={person.id}
-                  changed={(event) => this.nameChangedHandler(event, person.id)}
-                />)
-            })
-          }
-        </div>
+        <Persons persons={this.state.persons} 
+          clicked={this.deletePersonHandler} 
+          changed={this.nameChangedHandler} />
       );
-      buttonClass = classes.Red;
     }
-
-    const assignedClasses = [];
-
-    if (this.state.persons.length <= 2) {
-      assignedClasses.push(classes.red);
-    }
-    if (this.state.persons.length <= 1) {
-      assignedClasses.push(classes.bold);
-    }
-
-    let className = assignedClasses.join(' ');
 
     return (
       <div className={classes.App}>
-        <h1>Hi I'm a react app</h1>
-        <p className={className}>This is really working!</p>
-        <button 
-          onClick={this.togglePersonsHandler}
-          className={buttonClass}>
-          Toggle Persons
-        </button>
+        <Cockpit 
+          appTitle={this.props.title}
+          showPersons={this.state.showPersons}
+          persons={this.state.persons} 
+          clicked={this.togglePersonsHandler} />
         { persons }
       </div>
     );
